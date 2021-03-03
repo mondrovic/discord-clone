c3f9e4e1465b4225d40517164dff6f33911244b3
unknown <mondrovic1@gmail.com>
2021-03-03 16:05:57 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
-  <div className="app">
-    return <h2>Let's build a Discord Clone!!!</h2>;
-  </div>;
+  return (
+    <div className="App">
+      <h2>Let's build a discord clone</h2>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
